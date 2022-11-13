6c58bf4ff5205c1fd144fee0b73696377448d82d
Joe Torrance <joe_torrance@yahoo.com>
2022-11-13 16:02:20 -0500

    h1 blank after no el selected

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ console.log(divs);
 addEventListener("click", function(e){
     if(e.target === divs[0]){
         h1.innerText = "STOP!";
-        h1.style.color = "red";
+        h1.style.color = "rgba(255,0,0)";
     }
-    if(e.target === divs[1]){
+    else if(e.target === divs[1]){
         h1.innerText = "SLOW DOWN!";
-        h1.style.color = "yellow";
+        h1.style.color = "rgb(255,255,0)";
     }
-    if(e.target === divs[2]){
+    else if(e.target === divs[2]){
         h1.innerText = "GO! GO! GO!";
-        h1.style.color = "green";
+        h1.style.color = "rgb(0,255,0)";
     }
-    
+    else {
+        h1.innerText = "";
+    }
 })
